15660893994dbf543751dd8c73e878ebecc99862
ryanadams <rcoleman.adams@gmail.com>
2022-04-27 08:39:18 -0500

    commit tree fcd17a492c3235b5db8158433f65cd8c72dbeb76

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,11 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+  const focus = () => console.log('Good!');
+  const blurr = () => console.log('Hey! Eyes on me!')
+  return (
+    <button onFocus={focus} onBlur={blurr} >Eyes on me</button>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
-// Code Keypad Component Here
+import React from 'react'
+
+function Keypad() {
+  const logChange =(e)=> console.log(`Entering password...`)
+
+  return (
+    <input onChange={logChange} type='password' />
+  )
+}
+
+export default Keypad
